b6737026386d3e3fb959122d9d1c636934a6ba8d
Morten Winther Olsson <18525061+olwimo@users.noreply.github.com>
2023-03-13 12:28:07 +0100

    cleanup and yarn upgrade

--- src/Screen/Chat/Chat.tsx
+++ src/Screen/Chat/Chat.tsx
@@ -1,89 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {
-  createNativeStackNavigator,
   NativeStackScreenProps,
 } from '@react-navigation/native-stack';
 import {
   createDrawerNavigator,
-  DrawerScreenProps,
 } from '@react-navigation/drawer';
 
-import NavigationDrawerHeader from '../../Component/NavigationDrawerHeader';
 import CustomSidebarMenu from '../../Component/CustomSidebarMenu';
 import SettingsScreen from './SettingsScreen';
 import {RootStackParamList} from '../../App';
 import RoomScreen from './RoomScreen';
-
-// export type StackParamList = {
-//   RoomScreen: undefined;
-//   SettingsScreen: undefined;
-// };
 
 export type ChatStackParamList = {
   RoomScreen: undefined;
   SettingsScreen: undefined;
 };
 
-// const Stack = createNativeStackNavigator<StackParamList>();
 const Drawer = createDrawerNavigator<ChatStackParamList>();
-
-// const RoomScreenStack = ({
-//   navigation,
-// }: DrawerScreenProps<ChatStackParamList, 'RoomScreenStack'>) => {
-//   return (
-//     <Stack.Navigator
-//       initialRouteName="RoomScreen"
-//       screenOptions={{
-//         headerLeft: () => (
-//           <NavigationDrawerHeader navigationProps={navigation} />
-//         ),
-//         headerStyle: {
-//           backgroundColor: '#307ecc', //Set Header color
-//         },
-//         headerTintColor: '#fff', //Set Header text color
-//         headerTitleStyle: {
-//           fontWeight: 'bold', //Set Header text style
-//         },
-//       }}>
-//       <Stack.Screen
-//         name="RoomScreen"
-//         component={RoomScreen}
-//         options={{
-//           title: 'Chat', //Set Header Title
-//         }}
-//       />
-//     </Stack.Navigator>
-//   );
-// };
-
-// const SettingsScreenStack = ({
-//   navigation,
-// }: DrawerScreenProps<ChatStackParamList, 'SettingsScreenStack'>) => {
-//   return (
-//     <Stack.Navigator
-//       initialRouteName="SettingsScreen"
-//       screenOptions={{
-//         headerLeft: () => (
-//           <NavigationDrawerHeader navigationProps={navigation} />
-//         ),
-//         headerStyle: {
-//           backgroundColor: '#307ecc', //Set Header color
-//         },
-//         headerTintColor: '#fff', //Set Header text color
-//         headerTitleStyle: {
-//           fontWeight: 'bold', //Set Header text style
-//         },
-//       }}>
-//       <Stack.Screen
-//         name="SettingsScreen"
-//         component={SettingsScreen}
-//         options={{
-//           title: 'Settings', //Set Header Title
-//         }}
-//       />
-//     </Stack.Navigator>
-//   );
-// };
 
 const Chat = (_props: NativeStackScreenProps<RootStackParamList, 'Chat'>) => {
   return (
@@ -114,35 +47,5 @@ const Chat = (_props: NativeStackScreenProps<RootStackParamList, 'Chat'>) => {
     </Drawer.Navigator>
   );
 };
-
-// const Chat = (_props: NativeStackScreenProps<RootStackParamList, 'Chat'>) => {
-//   return (
-//     <Drawer.Navigator
-//       screenOptions={{
-//         headerShown: false,
-//         drawerActiveTintColor: '#cee1f2',
-//         drawerContentStyle: {shadowColor: '#cee1f2'},
-//         drawerItemStyle: {
-//           marginVertical: 5,
-//           borderColor: 'white',
-//         },
-//         drawerLabelStyle: {
-//           color: '#d8d8d8',
-//         },
-//       }}
-//       drawerContent={CustomSidebarMenu}>
-//       <Drawer.Screen
-//         name="SettingScreenStack"
-//         options={{drawerLabel: 'Settings'}}
-//         component={SettingScreenStack}
-//       />
-//       <Drawer.Screen
-//         name="RoomScreenStack"
-//         options={{drawerLabel: 'Chat'}}
-//         component={RoomScreenStack}
-//       />
-//     </Drawer.Navigator>
-//   );
-// };
 
 export default Chat;
